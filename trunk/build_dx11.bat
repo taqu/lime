7990ad7a42204dcaea6b847ccb0fd97984d5390f
taqu2920.dev <taqu2920.dev@6d1aec30-5835-e5f3-c638-b4d2469e39c8>
2015-04-27 16:55:08 +0000

    visual studio 2013

--- trunk/build_dx11.bat
+++ trunk/build_dx11.bat
@@ -1,6 +1,6 @@
 @echo off
 call "%VS110COMNTOOLS%vsvars32.bat"
 
-devenv lime_dx11_vc110.sln /Clean ReleaseSSE
+devenv lime_dx11_vc120.sln /Clean ReleaseSSE
 
-devenv lime_dx11_vc110.sln /Build ReleaseSSE
+devenv lime_dx11_vc120.sln /Build ReleaseSSE
--- trunk/build_dx11_d.bat
+++ trunk/build_dx11_d.bat
@@ -1,6 +1,6 @@
 @echo off
 call "%VS110COMNTOOLS%vsvars32.bat"
 
-devenv lime_dx11_vc110.sln /Clean DebugSSE
+devenv lime_dx11_vc120.sln /Clean DebugSSE
 
-devenv lime_dx11_vc110.sln /Build DebugSSE
+devenv lime_dx11_vc120.sln /Build DebugSSE
