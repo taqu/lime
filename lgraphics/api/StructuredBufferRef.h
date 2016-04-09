c8fcf3fc63f7e4a0835c85b1f1d8ad649be23094
taqu <taqu2920.dev@gmail.com>
2016-04-09 18:03:26 +0900

    StructuredBuffer interface

---
+++ lgraphics/api/StructuredBufferRef.h
@@ -0,0 +1,19 @@
+#ifndef INC_LGRAPHICS_STRUCTUREDBUFFERREF_H__
+#define INC_LGRAPHICS_STRUCTUREDBUFFERREF_H__
+/**
+@file StructuredBufferRef.h
+@author t-sakai
+@date 2015/12/22 create
+*/
+
+#if defined(LIME_DX9)
+
+#elif defined(LIME_DX11)
+#include "dx11/StructuredBufferRef.h"
+
+#elif defined(LIME_GLES1)
+
+#elif defined(LIME_GLES2)
+#endif
+
+#endif //INC_LGRAPHICS_STRUCTUREDBUFFERREF_H__
